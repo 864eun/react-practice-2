53e6f4447917e614392d84d6a8e596923c20beb4
eun <eun00864@gmail.com>
2025-05-09 00:59:33 +0900

    Add:section6 - state

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,6 +1,14 @@
-export default function Counter(){
+import { useState } from "react";
+
+export default function Counter({ontotal}){
+  const [counter, setCounter] = useState(1);
+
+  const handleCount = ()=>{
+    setCounter(counter+1);
+    ontotal();
+  }
   return(
-    <button>Counter</button>
+    <button onClick={handleCount}>Counter : {counter}</button>
   )
 };
 
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,8 +1,19 @@
+import { useState } from "react";
 import Counter from "./Counter";
 
 export default function Main() {
-  return(
-    <main><Counter /></main>
-  )
-}
+  const [total, setTotal] = useState(0);
+  const handleTotal = () => {
+    setTotal(total + 1);
+  };
 
+  return (
+    <main>
+      <h2>total: {total}</h2>
+      <Counter ontotal={handleTotal} />
+      <br />
+      <br />
+      <Counter ontotal={handleTotal} />
+    </main>
+  );
+}
